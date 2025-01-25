13ffdc558c40baeee1ed664a20b971568fb61826
Remidelgutte <delgutte.remi@hotmail.com>
2025-01-25 13:29:23 +0100

    add comment

--- file-postale.js
+++ file-postale.js
@@ -1,9 +1,16 @@
 class FilePostale {
   constructor() {
+    // Liste des clients en attente
     this.clients = [];
+    // Historique des clients servis
     this.historique = [];
   }
 
+  /**
+   * Ajoute un client à la file d'attente et trie la file
+   * en fonction des priorités (handicapé ou senior).
+   * @param {Client} client - Le client à ajouter.
+   */
   ajouterClient(client) {
     client.dateArrivee = new Date();
     this.clients.push(client);
@@ -25,6 +32,11 @@ class FilePostale {
     this.afficherFile();
   }
 
+  /**
+   * Sert le premier client de la file d'attente, le retire
+   * de la file, et l'ajoute à l'historique avec le temps
+   * d'attente calculé.
+   */
   servirClient() {
     if (this.clients.length === 0) {
       alert("Aucun client dans la file d'attente");
@@ -43,6 +55,7 @@ class FilePostale {
       : 0; // calcule le temps d'attente en secondes
     const tempsAttenteEnMinutes = Math.round(tempsAttente / 60); // convertit le temps d'attente en minutes et arrondit à l'entier le plus proche
 
+    // Détermine les catégories spéciales du client
     const categories = [];
     if (clientServi.handicape) {
       categories.push("Handicapé");
@@ -68,6 +81,10 @@ class FilePostale {
     this.afficherFile();
   }
 
+  /**
+   * Affiche la liste actuelle des clients en attente
+   * dans le tableau de la file d'attente.
+   */
   afficherFile() {
     const fileElement = document.getElementById("listeClients");
     fileElement.innerHTML = "";
@@ -84,6 +101,10 @@ class FilePostale {
     });
   }
 
+  /**
+   * Affiche l'historique des clients servis dans le
+   * tableau correspondant.
+   */
   afficherHistorique() {
     const historiqueElement = document.getElementById("listeHistorique");
     historiqueElement.innerHTML = "";
